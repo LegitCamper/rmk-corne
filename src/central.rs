0332a9a2028d5657a6ba58e9b10204bc366dce94
sawyer bristol <sawyerbristol@gmail.com>
2026-01-07 16:18:27 -0700

    init

--- src/central.rs
+++ src/central.rs
@@ -15,17 +15,21 @@ mod prospector;
 use crate::prospector::display::{ProspectorPins, create_display};
 
 use defmt::{info, unwrap};
-use embassy_executor::Spawner;
-use embassy_nrf::gpio::Output;
+use embassy_executor::{Executor, InterruptExecutor, Spawner};
+use embassy_nrf::interrupt::{InterruptExt, Priority};
 use embassy_nrf::mode::Async;
-use embassy_nrf::peripherals::{RNG, SPI3, USBD};
+use embassy_nrf::peripherals::{
+    NVMC, PPI_CH17, PPI_CH18, PPI_CH19, PPI_CH20, PPI_CH21, PPI_CH22, PPI_CH23, PPI_CH24, PPI_CH25,
+    PPI_CH26, PPI_CH27, PPI_CH28, PPI_CH29, PPI_CH30, PPI_CH31, RNG, SPI3, USBD,
+};
 use embassy_nrf::saadc::{self};
 use embassy_nrf::usb::Driver;
 use embassy_nrf::usb::vbus_detect::HardwareVbusDetect;
+use embassy_nrf::{Peri, interrupt};
 use embassy_nrf::{bind_interrupts, rng, spim, usb};
 use nrf_mpsl::Flash;
 use nrf_sdc::mpsl::MultiprotocolServiceLayer;
-use nrf_sdc::{self as sdc, mpsl};
+use nrf_sdc::{self as sdc, SoftdeviceController, mpsl};
 use rand_chacha::ChaCha12Rng;
 use rand_core::SeedableRng;
 use rmk::ble::build_ble_stack;
@@ -38,12 +42,14 @@ use rmk::keyboard::Keyboard;
 use rmk::split::ble::central::{read_peripheral_addresses, scan_peripherals};
 use rmk::split::central::run_peripheral_manager;
 use rmk::types::action::EncoderAction;
-use rmk::{HostResources, initialize_encoder_keymap_and_storage, run_rmk};
+use rmk::{
+    DefaultPacketPool, HostResources, Stack, initialize_encoder_keymap_and_storage, run_rmk,
+};
 use static_cell::StaticCell;
 
 use {defmt_rtt as _, panic_probe as _};
 
-static mut ARENA: [u8; 25 * 1024] = [0; 25 * 1024];
+static mut ARENA: [u8; 5 * 1024] = [0; 5 * 1024];
 
 #[global_allocator]
 static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> =
@@ -62,11 +68,6 @@ bind_interrupts!(struct Irqs {
     SPIM3 => spim::InterruptHandler<SPI3>;
 });
 
-#[embassy_executor::task]
-async fn mpsl_task(mpsl: &'static MultiprotocolServiceLayer<'static>) -> ! {
-    mpsl.run().await
-}
-
 /// How many outgoing L2CAP buffers per link
 const L2CAP_TXQ: u8 = 3;
 
@@ -76,12 +77,12 @@ const L2CAP_RXQ: u8 = 3;
 /// Size of L2CAP packets
 const L2CAP_MTU: usize = 251;
 
-fn build_sdc<'d, const N: usize>(
-    p: nrf_sdc::Peripherals<'d>,
-    rng: &'d mut rng::Rng<Async>,
-    mpsl: &'d MultiprotocolServiceLayer,
-    mem: &'d mut sdc::Mem<N>,
-) -> Result<nrf_sdc::SoftdeviceController<'d>, nrf_sdc::Error> {
+fn build_sdc<const N: usize>(
+    p: nrf_sdc::Peripherals<'static>,
+    rng: &'static mut rng::Rng<Async>,
+    mpsl: &'static MultiprotocolServiceLayer,
+    mem: &'static mut sdc::Mem<N>,
+) -> Result<nrf_sdc::SoftdeviceController<'static>, nrf_sdc::Error> {
     sdc::Builder::new()?
         .support_scan()?
         .support_central()?
@@ -106,17 +107,49 @@ fn ble_addr() -> [u8; 6] {
     unwrap!(addr.to_le_bytes()[..6].try_into())
 }
 
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    info!("Hello RMK BLE!");
-    // Initialize the peripherals and nrf-sdc controller
-    let mut nrf_config = embassy_nrf::config::Config::default();
-    nrf_config.dcdc.reg0_voltage = Some(embassy_nrf::config::Reg0Voltage::_3V3);
-    nrf_config.dcdc.reg0 = true;
-    nrf_config.dcdc.reg1 = true;
-    let p = embassy_nrf::init(nrf_config);
-    let mpsl_p =
-        mpsl::Peripherals::new(p.RTC0, p.TIMER0, p.TEMP, p.PPI_CH19, p.PPI_CH30, p.PPI_CH31);
+static EXECUTOR_MED: InterruptExecutor = InterruptExecutor::new();
+static EXECUTOR_LOW: StaticCell<Executor> = StaticCell::new();
+
+struct MpslPeris {
+    rtc: Peri<'static, embassy_nrf::peripherals::RTC0>,
+    timer: Peri<'static, embassy_nrf::peripherals::TIMER0>,
+    timer1: Peri<'static, embassy_nrf::peripherals::TEMP>,
+    ch19: Peri<'static, PPI_CH19>,
+    ch30: Peri<'static, PPI_CH30>,
+    ch31: Peri<'static, PPI_CH31>,
+}
+
+struct SdcPeris {
+    ppi17: Peri<'static, PPI_CH17>,
+    ppi18: Peri<'static, PPI_CH18>,
+    ppi20: Peri<'static, PPI_CH20>,
+    ppi21: Peri<'static, PPI_CH21>,
+    ppi22: Peri<'static, PPI_CH22>,
+    ppi23: Peri<'static, PPI_CH23>,
+    ppi24: Peri<'static, PPI_CH24>,
+    ppi25: Peri<'static, PPI_CH25>,
+    ppi26: Peri<'static, PPI_CH26>,
+    ppi27: Peri<'static, PPI_CH27>,
+    ppi28: Peri<'static, PPI_CH28>,
+    ppi29: Peri<'static, PPI_CH29>,
+}
+
+#[embassy_executor::task]
+async fn run_med(
+    mpsl_peris: MpslPeris,
+    sdc_peris: SdcPeris,
+    rng: Peri<'static, RNG>,
+    nvmc: Peri<'static, NVMC>,
+    usbd: Peri<'static, USBD>,
+) -> ! {
+    let mpsl_p = mpsl::Peripherals::new(
+        mpsl_peris.rtc,
+        mpsl_peris.timer,
+        mpsl_peris.timer1,
+        mpsl_peris.ch19,
+        mpsl_peris.ch30,
+        mpsl_peris.ch31,
+    );
     let lfclk_cfg = mpsl::raw::mpsl_clock_lfclk_cfg_t {
         source: mpsl::raw::MPSL_CLOCK_LF_SRC_RC as u8,
         rc_ctiv: mpsl::raw::MPSL_RECOMMENDED_RC_CTIV as u8,
@@ -132,23 +165,34 @@ async fn main(spawner: Spawner) {
         lfclk_cfg,
         SESSION_MEM.init(mpsl::SessionMem::new())
     )));
-    spawner.must_spawn(mpsl_task(&*mpsl));
     let sdc_p = sdc::Peripherals::new(
-        p.PPI_CH17, p.PPI_CH18, p.PPI_CH20, p.PPI_CH21, p.PPI_CH22, p.PPI_CH23, p.PPI_CH24,
-        p.PPI_CH25, p.PPI_CH26, p.PPI_CH27, p.PPI_CH28, p.PPI_CH29,
+        sdc_peris.ppi17,
+        sdc_peris.ppi18,
+        sdc_peris.ppi20,
+        sdc_peris.ppi21,
+        sdc_peris.ppi22,
+        sdc_peris.ppi23,
+        sdc_peris.ppi24,
+        sdc_peris.ppi25,
+        sdc_peris.ppi26,
+        sdc_peris.ppi27,
+        sdc_peris.ppi28,
+        sdc_peris.ppi29,
     );
-    let mut rng = rng::Rng::new(p.RNG, Irqs);
-    let mut rng_gen = ChaCha12Rng::from_rng(&mut rng).unwrap();
-    let mut sdc_mem = sdc::Mem::<15472>::new();
-    let sdc = unwrap!(build_sdc(sdc_p, &mut rng, mpsl, &mut sdc_mem));
+    static RNG: StaticCell<rng::Rng<'static, Async>> = StaticCell::new();
+    let rng = RNG.init(rng::Rng::new(rng, Irqs));
+    let mut rng_gen = ChaCha12Rng::from_rng(&mut *rng).unwrap();
+    static SDC_MEM: StaticCell<sdc::Mem<15472>> = StaticCell::new();
+    let sdc_mem = SDC_MEM.init(sdc::Mem::<15472>::new());
+    let sdc = unwrap!(build_sdc(sdc_p, rng, mpsl, sdc_mem));
     let mut host_resources = HostResources::new();
     let stack = build_ble_stack(sdc, ble_addr(), &mut rng_gen, &mut host_resources).await;
 
     // Initialize usb driver
-    let driver = Driver::new(p.USBD, Irqs, HardwareVbusDetect::new(Irqs));
+    let driver = Driver::new(usbd, Irqs, HardwareVbusDetect::new(Irqs));
 
     // Initialize flash
-    let flash = Flash::take(mpsl, p.NVMC);
+    let flash = Flash::take(mpsl, nvmc);
 
     // Keyboard config
     let keyboard_device_config = DeviceConfig {
@@ -200,8 +244,71 @@ async fn main(spawner: Spawner) {
     let peripheral_addrs =
         read_peripheral_addresses::<2, _, ROW, COL, NUM_LAYER, 0>(&mut storage).await;
 
-    // create prospector display
-    let (display, _backlight_pin, framebuffer) = create_display(ProspectorPins {
+    // Start
+    join3(
+        mpsl.run(),
+        keyboard.run(),
+        join4(
+            scan_peripherals(&stack, &peripheral_addrs),
+            run_peripheral_manager::<ROW, COL, 0, 0, _>(0, &peripheral_addrs, &stack),
+            run_peripheral_manager::<ROW, COL, 0, 6, _>(1, &peripheral_addrs, &stack),
+            run_rmk(&keymap, driver, &stack, &mut storage, rmk_config),
+        ),
+    )
+    .await;
+
+    loop {}
+}
+
+#[embassy_executor::task]
+async fn run_low(pins: ProspectorPins) {
+    let (display, _backlight_pin, framebuffer) = create_display(pins).await;
+    prospector::run(display, framebuffer).await
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    info!("Hello RMK BLE!");
+
+    // Initialize the peripherals and nrf-sdc controller
+    let mut nrf_config = embassy_nrf::config::Config::default();
+    nrf_config.dcdc.reg0_voltage = Some(embassy_nrf::config::Reg0Voltage::_3V3);
+    nrf_config.dcdc.reg0 = true;
+    nrf_config.dcdc.reg1 = true;
+    let p = embassy_nrf::init(nrf_config);
+
+    let mpsl_peris = MpslPeris {
+        rtc: p.RTC0,
+        timer: p.TIMER0,
+        timer1: p.TEMP,
+        ch19: p.PPI_CH19,
+        ch30: p.PPI_CH30,
+        ch31: p.PPI_CH31,
+    };
+
+    let sdc_peris = SdcPeris {
+        ppi17: p.PPI_CH17,
+        ppi18: p.PPI_CH18,
+        ppi20: p.PPI_CH20,
+        ppi21: p.PPI_CH21,
+        ppi22: p.PPI_CH22,
+        ppi23: p.PPI_CH23,
+        ppi24: p.PPI_CH24,
+        ppi25: p.PPI_CH25,
+        ppi26: p.PPI_CH26,
+        ppi27: p.PPI_CH27,
+        ppi28: p.PPI_CH28,
+        ppi29: p.PPI_CH29,
+    };
+
+    // Medium-priority executor: EGU0_SWI0, priority level 7
+    interrupt::EGU0_SWI0.set_priority(Priority::P7);
+    let spawner = EXECUTOR_MED.start(interrupt::EGU0_SWI0);
+    spawner
+        .spawn(run_med(mpsl_peris, sdc_peris, p.RNG, p.NVMC, p.USBD))
+        .unwrap();
+
+    let prospector_pins = ProspectorPins {
         spi: p.SPI3,
         dc: p.P1_12,
         sck: p.P1_13,
@@ -209,19 +316,11 @@ async fn main(spawner: Spawner) {
         mosi: p.P1_15,
         bl: p.P1_11,
         rst: p.P0_29,
-    })
-    .await;
+    };
 
-    // Start
-    join(
-        // keyboard.run(),
-        join4(
-            scan_peripherals(&stack, &peripheral_addrs),
-            run_peripheral_manager::<ROW, COL, 0, 0, _>(0, &peripheral_addrs, &stack),
-            run_peripheral_manager::<ROW, COL, 0, 6, _>(1, &peripheral_addrs, &stack),
-            run_rmk(&keymap, driver, &stack, &mut storage, rmk_config),
-        ),
-        prospector::run(display, framebuffer),
-    )
-    .await;
+    // Low priority executor: runs in thread mode, using WFE/SEV
+    let executor = EXECUTOR_LOW.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(run_low(prospector_pins)).unwrap();
+    });
 }
